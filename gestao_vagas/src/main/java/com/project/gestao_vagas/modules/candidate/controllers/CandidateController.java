8ad820a7a753874d43f87e15a972e420a6fa85fa
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2024-03-04 20:40:20 -0300

    Update Lombok

---
+++ gestao_vagas/src/main/java/com/project/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -0,0 +1,15 @@
+package com.project.gestao_vagas.modules.candidate.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/candidate")
+public class CandidateController {
+    
+    @PostMapping("/")
+    public void create(){
+        
+    }
+}
