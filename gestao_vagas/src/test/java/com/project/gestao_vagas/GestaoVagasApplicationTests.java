c6e3ce4c1e3ca1ecc1584b349982f1446a3b0e7d
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2024-03-04 20:18:21 -0300

    Create_project

---
+++ gestao_vagas/src/test/java/com/project/gestao_vagas/GestaoVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.gestao_vagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
