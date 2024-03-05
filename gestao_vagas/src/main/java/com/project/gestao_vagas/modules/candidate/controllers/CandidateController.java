89157a457f4149ad7baeb49ec6a5dc1f68325d96
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2024-03-04 21:12:48 -0300

    Update Postman

--- gestao_vagas/src/main/java/com/project/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ gestao_vagas/src/main/java/com/project/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,15 +1,19 @@
 package com.project.gestao_vagas.modules.candidate.controllers;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.project.gestao_vagas.modules.candidate.CandidateEntity;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
     
     @PostMapping("/")
-    public void create(){
-        
+    public void create(@RequestBody CandidateEntity candidateEntity){
+        System.out.println("Candidato");
+        System.out.println(candidateEntity.getEmail());
     }
 }
