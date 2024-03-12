025bbdb6635845a8f8fd1ed1e6c0419bfc928878
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2024-03-12 17:55:30 -0300

    Update Codigo and conection postbrid

--- gestao_vagas/src/main/java/com/project/gestao_vagas/modules/candidate/CandidateEntity.java
+++ gestao_vagas/src/main/java/com/project/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,18 +1,28 @@
 package com.project.gestao_vagas.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+    
     private String name;
 
     @Pattern(regexp = "\\S+", message = "O campo [username] não pode conter espaço")
@@ -23,62 +33,11 @@ public class CandidateEntity {
 
     @Length(min = 6, max = 15, message = "A senha deve conter entre 6 e 15 caracteries")
     private String password;
+
     private String description;
+
     private String curriculum;
-    
 
-    // public UUID getId() {
-    //     return id;
-    // }
-    // public void setId(UUID id) {
-    //     this.id = id;
-    // }
-
-
-    // public String getName() {
-    //     return name;
-    // }
-    // public void setName(String name) {
-    //     this.name = name;
-    // }
-
-
-    // public String getUsername() {
-    //     return username;
-    // }
-    // public void setUsername(String username) {
-    //     this.username = username;
-    // }
-
-
-    // public String getEmail() {
-    //     return email;
-    // }
-    // public void setEmail(String email) {
-    //     this.email = email;
-    // }
-
-
-    // public String getPassword() {
-    //     return password;
-    // }
-    // public void setPassword(String password) {
-    //     this.password = password;
-    // }
-
-
-    // public String getDescription() {
-    //     return description;
-    // }
-    // public void setDescription(String description) {
-    //     this.description = description;
-    // }
-
-
-    // public String getCurriculum() {
-    //     return curriculum;
-    // }
-    // public void setCurriculum(String curriculum) {
-    //     this.curriculum = curriculum;
-    // }
+    @CreationTimestamp
+    private LocalDateTime cratedAt;
 }
