81996347f9ad6e9a1ca4ead0ddde0039f9daafbe
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2024-03-13 18:59:37 -0300

    Update useCases

--- gestao_vagas/src/main/java/com/project/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ gestao_vagas/src/main/java/com/project/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,14 +1,14 @@
 package com.project.gestao_vagas.modules.candidate.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.project.gestao_vagas.exceptions.UserFoundException;
 import com.project.gestao_vagas.modules.candidate.CandidateEntity;
-import com.project.gestao_vagas.modules.candidate.CandidateRepository;
+import com.project.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
 
 import jakarta.validation.Valid;
 
@@ -17,15 +17,18 @@ import jakarta.validation.Valid;
 public class CandidateController {
     
     @Autowired
-    private CandidateRepository candidateRepository;
-    
-    @PostMapping("/")
-    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity){
+    private CreateCandidateUseCase createCandidateUseCase;
 
-        this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
-        .ifPresent((user) -> {
-            throw new UserFoundException();
-        });
-       return this.candidateRepository.save(candidateEntity);
+    @PostMapping("/")
+    public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity){
+        
+        try {
+            var result = this.createCandidateUseCase.execute(candidateEntity);
+            return ResponseEntity.ok().body(result);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+        
+        
     }
 }
