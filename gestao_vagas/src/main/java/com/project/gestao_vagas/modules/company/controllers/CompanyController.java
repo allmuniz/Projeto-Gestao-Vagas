580e5d87b9ba9798de0467a09cf80a73624040d7
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2024-03-15 18:06:38 -0300

    Create CompanyController

--- gestao_vagas/src/main/java/com/project/gestao_vagas/modules/company/controllers/CompanyController.java
+++ gestao_vagas/src/main/java/com/project/gestao_vagas/modules/company/controllers/CompanyController.java
@@ -1,4 +1,4 @@
-package com.project.gestao_vagas.modules.candidate.controllers;
+package com.project.gestao_vagas.modules.company.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -7,27 +7,27 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.project.gestao_vagas.modules.candidate.CandidateEntity;
-import com.project.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
+import com.project.gestao_vagas.modules.company.entities.CompanyEntity;
+import com.project.gestao_vagas.modules.company.useCases.CreateCompanyUseCase;
 
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/candidate")
-public class CandidateController {
+@RequestMapping("/company")
+public class CompanyController {
     
     @Autowired
-    private CreateCandidateUseCase createCandidateUseCase;
+    private CreateCompanyUseCase createCompanyUseCase;
 
     @PostMapping("/")
-    public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity){
+    public ResponseEntity<Object> create(@Valid @RequestBody CompanyEntity companyEntity){
         
         try {
-            var result = this.createCandidateUseCase.execute(candidateEntity);
+            var result = this.createCompanyUseCase.execute(companyEntity);
             return ResponseEntity.ok().body(result);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
-          
+         
     }
 }
