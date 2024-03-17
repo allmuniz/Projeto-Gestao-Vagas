f836796b39385ad1dfbb14d6583b65fac437a109
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2024-03-17 08:45:59 -0300

    Update SecurityConfig.java

--- gestao_vagas/src/main/java/com/project/gestao_vagas/security/SecurityConfig.java
+++ gestao_vagas/src/main/java/com/project/gestao_vagas/security/SecurityConfig.java
@@ -11,7 +11,15 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 
-        http.csrf(csrf -> csrf.disable());
+        http.csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth ->{
+                auth.requestMatchers("/candidate/").permitAll()
+                .requestMatchers("/company/").permitAll();
+                
+                auth.anyRequest().authenticated();
+            })
+        
+        ;
         return http.build();
     }
 }
