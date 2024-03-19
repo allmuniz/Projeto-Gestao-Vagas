bc77cbafaa09a5c2537a390fcde735c42b8d0913
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2024-03-19 11:22:57 -0300

    Update JobEntity.java

--- gestao_vagas/src/main/java/com/project/gestao_vagas/modules/company/entities/JobEntity.java
+++ gestao_vagas/src/main/java/com/project/gestao_vagas/modules/company/entities/JobEntity.java
@@ -6,7 +6,6 @@ import java.util.UUID;
 import org.hibernate.annotations.CreationTimestamp;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
